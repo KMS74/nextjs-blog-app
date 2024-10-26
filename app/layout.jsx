e3a604144bfd6e25a097923f4f400e943d57f511
Karem Shabana <km425360@gmail.com>
2024-10-26 17:29:31 +0300

    Refactor layout component to add language and direction attributes to the HTML tag

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,15 @@
 import "../styles/globals.css";
 
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function Layout({ children }) {
   return (
-    <html lang="en" dir="ltr">
+    <html lang="en" dir="ltr" className={inter.className}>
       <body>{children}</body>
     </html>
   );
